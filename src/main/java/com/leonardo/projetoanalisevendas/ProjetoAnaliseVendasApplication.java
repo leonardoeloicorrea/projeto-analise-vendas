70d1f2f2d3b7e28d1bcbce419558f0eb54fb3d05
Leonarco correa <leonardoeloi24@gmail.com>
2023-05-20 15:50:16 -0300

    Created project and h2 config

---
+++ src/main/java/com/leonardo/projetoanalisevendas/ProjetoAnaliseVendasApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.projetoanalisevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAnaliseVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAnaliseVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/projetoanalisevendas/ProjetoAnaliseVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.projetoanalisevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAnaliseVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
